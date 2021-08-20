6fc57f795fc1dfa929279479731f2e5ff1a5e219
bohdan <bohdan.korolev@gmail.com>
2021-08-20 12:53:49 +0300

    Add Bootstrap to the project Add page components (home, dashboard, auth, registration) Add navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./components/pages/home/home.component";
+import {RegistrationComponent} from "./components/pages/registration/registration.component";
+import {AuthComponent} from "./components/pages/auth/auth.component";
+import {DashboardComponent} from "./components/pages/dashboard/dashboard.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'registration',
+    component: RegistrationComponent
+  },
+  {
+    path: 'authenticate',
+    component: AuthComponent
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { HeaderComponent } from './components/common/header/header.component';
+import { RegistrationComponent } from './components/pages/registration/registration.component';
+import { AuthComponent } from './components/pages/auth/auth.component';
+import { DashboardComponent } from './components/pages/dashboard/dashboard.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FooterComponent } from './components/common/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    RegistrationComponent,
+    AuthComponent,
+    DashboardComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
